7f425c88012d57f1b63748f4234303777b98ebb8
Blaz Solar <solarblaz@gmail.com>
2017-08-05 18:29:10 +0200

    Typo

--- config/ci/run_emulator.sh
+++ config/ci/run_emulator.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$ANDROID_DEVICE" != "static" ]
 then
-  echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -f -n emulator -k "system-images;$ANDROID_TARGET;$$ANDROID_DEVICE;$ANDROID_ABI"
+  echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -f -n emulator -k "system-images;$ANDROID_TARGET;$ANDROID_DEVICE;$ANDROID_ABI"
   $ANDROID_HOME/emulator/emulator -avd emulator -no-window -no-audio -wipe-data -no-boot-anim &
   android-wait-for-emulator
   adb devices
